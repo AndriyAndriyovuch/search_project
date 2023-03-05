ce2e8505efcfba121a554e0937735c3afb38396b
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-03-05 17:16:16 +0200

    add sort by relevance and divide methods

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,13 @@ class IndexController < ApplicationController
   def search
     return unless params[:query].present?
 
-    params[:match] == '1' ? exact_match : regular_match
+    if params[:match] == '1'
+      params[:negative] == '1' ? exact_negative_match : exact_match
+    else
+      params[:negative] == '1' ? regular_negative_match : regular_match
+    end
+
+    sort_by_relevance if params[:negative] == '1'
   end
 
   private
@@ -12,28 +18,54 @@ class IndexController < ApplicationController
     query_data = params[:query].downcase
 
     # collect all results by join json values to string and find if any value include string
-    @results = DATA_HASH.select do |data|
-      case params[:negative] # Negative search
-      when '1'
-        data unless data.values.map(&:downcase).join(' ').include? query_data
-      else
-        data if data.values.map(&:downcase).join(' ').include? query_data
-      end
+    @results = DATA_HASH.select do |result|
+      results_string = result.values.map(&:downcase).join(' ') # map all values into string
+      result if results_string.include? query_data
     end
   end
 
   def regular_match
-    query_data = params[:query].downcase.split # Split query text to match with every single word
+    # Split query text to match with every single word
+    query_data = params[:query].downcase.split
 
-    query_data.each do |word| # Iterate every query word to search in data
-      @results = DATA_HASH.select do |data|
-        case params[:negative] # Negative search
-        when '1'
-          data unless data.values.map(&:downcase).join(' ').include? word # map values into a string and search matches
-        else
-          data if data.values.map(&:downcase).join(' ').include? word
-        end
+    # Iterate every query word to search in data
+    query_data.each do |word|
+      @results = DATA_HASH.select do |result|
+        results_string = result.values.map(&:downcase).join(' ') # map all values into string
+        result if results_string.include? word
       end
     end
+    @results
+  end
+
+  def exact_negative_match
+    query_data = params[:query].downcase
+    @results = DATA_HASH
+
+    # delete result from results if there is exact match
+    @results.delete_if { |result| result.values.map(&:downcase).join(' ').include? query_data }
+  end
+
+  def regular_negative_match
+    @results = DATA_HASH
+
+    # Split query text to match with every single word
+    query_data = params[:query].downcase.split
+
+    # Iterate every query word to search in data and delete from results if word matches
+    query_data.each do |word|
+      @results.delete_if { |result| result.values.map(&:downcase).join(' ').include? word }
+    end
+  end
+
+  def sort_by_relevance
+    # Compare length of query and data
+    @results.map do |result|
+      result['relevance'] = (result.values.map(&:downcase).join(' ').length - params[:query].length).to_s
+    end
+
+    # Sort by relevance and delete relevence field after it (only in positive search)
+    @results.sort_by! { |result| result['relevance'].to_i }
+    @results.map { |result| result.delete('relevance') }
   end
 end
