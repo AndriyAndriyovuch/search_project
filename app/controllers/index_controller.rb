258696f3a321213d1a23ac9c2a03b00d37c8800c
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-03-05 15:14:22 +0200

    add exact match for search

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,13 +1,29 @@
 class IndexController < ApplicationController
+  DATA_HASH = JSON.parse(File.read('config/languages.json'))
   def search
-    data_hash = JSON.parse(File.read('config/languages.json'))
-
     if params[:query].present?
-      query_data = params[:query].downcase
+      params[:match] == '1' ? exact_match : regular_match
+    end
 
-      #collect all results by join json values to string and find if any value include string
-      @results = data_hash.select do | data |
-        data if data.values.map { | key | key.downcase }.join(' ').include? query_data
+
+  end
+
+  private
+  def exact_match
+    query_data = params[:query].downcase
+
+    #collect all results by join json values to string and find if any value include string
+    @results = DATA_HASH.select do | data |
+      data if data.values.map { | key | key.downcase }.join(' ').include? query_data
+    end
+  end
+
+  def regular_match
+    query_data = params[:query].downcase.split(' ') # Split query text to match with every single word
+
+    query_data.each do | word | # Iterate every query word to search in data
+      @results = DATA_HASH.select do | data |
+        data if data.values.map { | key | key.downcase }.join(' ').include? word # map all values into a string and search matches
       end
     end
   end
