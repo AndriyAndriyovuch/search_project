8c076a4128b934e3a39f0c76fbe622c59b363e4a
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-03-05 15:39:53 +0200

    use Rubocop

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,29 +1,38 @@
 class IndexController < ApplicationController
   DATA_HASH = JSON.parse(File.read('config/languages.json'))
   def search
-    if params[:query].present?
-      params[:match] == '1' ? exact_match : regular_match
-    end
+    return unless params[:query].present?
 
-
+    params[:match] == '1' ? exact_match : regular_match
   end
 
   private
+
   def exact_match
     query_data = params[:query].downcase
 
-    #collect all results by join json values to string and find if any value include string
-    @results = DATA_HASH.select do | data |
-      data if data.values.map { | key | key.downcase }.join(' ').include? query_data
+    # collect all results by join json values to string and find if any value include string
+    @results = DATA_HASH.select do |data|
+      case params[:negative] # Negative search
+      when '1'
+        data unless data.values.map(&:downcase).join(' ').include? query_data
+      else
+        data if data.values.map(&:downcase).join(' ').include? query_data
+      end
     end
   end
 
   def regular_match
-    query_data = params[:query].downcase.split(' ') # Split query text to match with every single word
+    query_data = params[:query].downcase.split # Split query text to match with every single word
 
-    query_data.each do | word | # Iterate every query word to search in data
-      @results = DATA_HASH.select do | data |
-        data if data.values.map { | key | key.downcase }.join(' ').include? word # map all values into a string and search matches
+    query_data.each do |word| # Iterate every query word to search in data
+      @results = DATA_HASH.select do |data|
+        case params[:negative] # Negative search
+        when '1'
+          data unless data.values.map(&:downcase).join(' ').include? word # map values into a string and search matches
+        else
+          data if data.values.map(&:downcase).join(' ').include? word
+        end
       end
     end
   end
